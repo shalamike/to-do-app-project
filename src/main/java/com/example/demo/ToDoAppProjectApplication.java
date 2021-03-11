687778f61c26101733620b06f226501d695c5916
shalamike <mshala02@dcs.bbk.ac.uk>
2021-03-11 12:03:52 +0000

    first commit

---
+++ src/main/java/com/example/demo/ToDoAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppProjectApplication.class, args);
+	}
+
+}
