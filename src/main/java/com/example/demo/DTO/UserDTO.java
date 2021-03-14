12995dd92f9189ee37b02a14a97c551c81e737e8
shalamike <mshala02@dcs.bbk.ac.uk>
2021-03-14 01:08:16 +0000

    TDLFW-23 added DTO for user

---
+++ src/main/java/com/example/demo/DTO/UserDTO.java
@@ -0,0 +1,100 @@
+package com.example.demo.DTO;
+
+public class UserDTO {
+	
+	private int userId;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String userName;
+	
+	private String email;
+	
+	private String password;
+	
+	public UserDTO(int userId, String firstName, String lastName, private String userName ,String email){
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.email = email;
+	}
+	
+	public UserDTO(int userId, String firstName, String lastName, private String userName ,String email, String password){
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.email = email;
+		this.password = password;
+	}
+	
+	public String getIdentification() {
+		return id + " " + userName + " " + email;
+		
+	}
+	
+	public void setIdentification(String in) {
+		String[] identification = in.split(" ");
+		this.userId = Integer.parseInt(identification[0]);
+		this.userName = identification[1];
+		this.email = identification[2];
+	}
+	
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDTO [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", userName="
+				+ userName + ", email=" + email + "]";
+	}
+}
--- src/main/java/com/example/demo/data/repository/UserRepository.java
+++ src/main/java/com/example/demo/data/repository/UserRepository.java
@@ -1,5 +1,25 @@
 package com.example.demo.data.repository;
 
-public interface UserRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.data.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+	
+//	//try finding the user name using this
+//	public User findbyUserName(String name);
+	
+//	//or this
+//	@Query("SELECT u from user u where u.user_name = ?1")
+//	public User findbyUserNameJPQL(String name);
+//	need to fix one
+	
+	//selecting everything from user
+	@Query(value = "SELECT * FROM user", nativeQuery = true)
+	public List<User> getAllUsersSQL();
 }
