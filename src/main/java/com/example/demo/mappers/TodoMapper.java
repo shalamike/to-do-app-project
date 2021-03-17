f757df42e57f857cbe997454ed7eb4ea236cbcca
shalamike <mshala02@dcs.bbk.ac.uk>
2021-03-17 15:23:25 +0000

    TDLFW-35 created mappers for the todos and users

--- src/main/java/com/example/demo/mappers/TodoMapper.java
+++ src/main/java/com/example/demo/mappers/TodoMapper.java
@@ -1,5 +1,28 @@
 package com.example.demo.mappers;
 
-public class TodoMapper {
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.example.demo.DTO.TodoDTO;
+import com.example.demo.DTO.UserDTO;
+import com.example.demo.data.model.Todo;
+
+@Component
+public class TodoMapper {
+	
+	private ModelMapper modelMapper;
+	
+	@Autowired
+	public TodoMapper(ModelMapper modelMapper) {
+		this.modelMapper = modelMapper;
+	}
+	
+	public TodoDTO mapToDTO(Todo todo) {
+		return this.modelMapper.map(todo, TodoDTO.class);
+	}
+	
+	public Todo mapToTodo(TodoDTO todoDTO) {
+		return this.modelMapper.map(todoDTO, Todo.class);
+	}
 }
