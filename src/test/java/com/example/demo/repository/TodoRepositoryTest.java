a203f5911a4ecd0c10625face6107fec89bcb1e5
shalamike <mshala02@dcs.bbk.ac.uk>
2021-03-17 23:13:39 +0000

    TDLFW-25 created unit test for todo repository

---
+++ src/test/java/com/example/demo/repository/TodoRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.demo.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.example.demo.data.repository.TodoRepository;
+
+@DataJpaTest
+public class TodoRepositoryTest {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	private EntityManager entityManager;
+	
+	@Autowired
+	private TodoRepository todoRepository;
+	
+	@Test 
+	void testInjectedComponentsAreNotNull() {
+		assertThat(dataSource).isNotNull();
+	    assertThat(dataSource).isNotNull();
+	    assertThat(jdbcTemplate).isNotNull();
+	    assertThat(entityManager).isNotNull();
+	    assertThat(todoRepository).isNotNull();
+		
+	}
+
+}
